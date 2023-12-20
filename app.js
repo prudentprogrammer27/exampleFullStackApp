d24b7204228256eb7306f7aa51f9c839799a73c3
prudentprogrammer27 <declercq008@gmail.com>
2023-12-20 15:32:29 -0700

    .env and stuff

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 import express from 'express';
 import path from 'path';
 import pg from 'pg';
+import 'dotenv/config';
 
 const { Pool } = pg;
 const app = express();
 
 const expressPort = 8002;
 
+const connectionString = process.env.DATABASE_URL;
+
 const pool = new Pool ({
-  user: 'kevingoble',
-  host: 'localhost',
-  database: 'items',
-  port: 5432
+  connectionString,
+  // user: 'kevingoble',
+  // host: 'postgres://galvanize:Ki3NrzP5kzGbxF1Qa3lTNc5opa6TgnyB@dpg-cm1lngi1hbls73bp8ok0-a.oregon-postgres.render.com/items_kgqg',
+  // password: 'Ki3NrzP5kzGbxF1Qa3lTNc5opa6TgnyB'
+  // database: 'items_kgqg',
+  // port: 5432
 });
 
 app.use(express.static('public'));
